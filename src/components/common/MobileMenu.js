a88c159e857ebfa280a0baa2fa7eaf104df4bbec
openkmj <42940044+openkmj@users.noreply.github.com>
2020-11-10 01:01:13 +0900

    메뉴 아이콘 수정, 크기 조정

--- src/components/common/MobileMenu.js
+++ src/components/common/MobileMenu.js
@@ -6,7 +6,7 @@ function MobileMenu() {
         <div className="sl-m-header-wrapper">
             
             <div className="sl-m-header-menu-button">
-                <img src="img/icon/ic-person.png" alt="메뉴"/>
+                <img src="img/icon/ic-menu.png" alt="메뉴"/>
             </div>
             <div className="sl-m-header-logo">
                 <img src="img/logo/logo.png" alt="소셜라운지 로고" />
